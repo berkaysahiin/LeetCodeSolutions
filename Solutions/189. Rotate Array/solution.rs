1ec22afce5aefe4d4da4466663515ca6c6a30643
berkaysahiin <bbberkaysahin@gmail.com>
2023-02-10 19:29:43 +0300

    189. Rotate Array

---
+++ Solutions/189. Rotate Array/solution.rs
@@ -0,0 +1,55 @@
+/*
+
+189. Rotate Array
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+    Input: nums = [-1,-100,3,99], k = 2
+    Output: [3,99,-1,-100]
+    Explanation: 
+    rotate 1 steps to the right: [99,-1,-100,3]
+    rotate 2 steps to the right: [3,99,-1,-100]
+
+Constraints:
+    1 <= nums.length <= 105
+    -231 <= nums[i] <= 231 - 1
+    0 <= k <= 105
+*/
+
+use std::collections::VecDeque;
+
+
+impl Solution {
+    pub fn rotate(nums: &mut Vec<i32>, mut k: i32) {
+        let mut nums_deque: VecDeque<i32> = VecDeque::new();
+
+        k %= nums.len() as i32;
+
+        for i in 0..k {
+          match nums.pop() {
+            Some(val) => nums_deque.push_front(val),
+            None => break,
+          }
+        }
+
+        nums.reverse();
+
+        loop {
+          match nums.pop() {
+            Some(val) => nums_deque.push_back(val),
+            None => break,
+          }
+        }
+
+      *nums = Vec::from(nums_deque);
+    }
+}
